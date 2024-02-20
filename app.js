83023f28b7d4cdda080ddcb6cde288b1983ab2b4
pintu8328 <97545631+pintu8328@users.noreply.github.com>
2024-02-21 02:02:54 +0530

    router created

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require('http');
-const server=http.createServer((req,res)=>{
-    console.log("Pintu Gouda")
-    res.end("Pintu Gouda")
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/home') {
+    res.end('Welcome home');
+  } else if (url === '/about') {
+    res.end('Welcome to About Us page');
+  } else if (url === '/node') {
+    res.end('Welcome to my Node.js project');
+  } else {
+    res.end('Page not found');
+  }
 });
- 
-server.listen(3000,()=>{
-    console.log("server is running on port 3000")
-})
+
+server.listen(3000, () => {
+  console.log('Server running on port 4000');
+});
